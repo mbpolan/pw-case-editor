b2235392a68b053a9eda8cbb9ecaebd6f1c9e2ef
Mike Polan <mbpolan@gmail.com>
2008-01-22 22:31:29 +0000

    -Added missing files

---
+++ pw_case_editor/src/editdialogs.h
@@ -0,0 +1,80 @@
+/***************************************************************************
+ *   Copyright (C) 2007 by Mike Polan                                      *
+ *   kanadakid@gmail.com                                                   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+// editdialogs.h: various Edit menu based dialogs
+
+#ifndef EDITDIALOGS_H
+#define EDITDIALOGS_H
+
+#include <gtkmm/button.h>
+#include <gtkmm/dialog.h>
+#include <gtkmm/entry.h>
+#include <gtkmm/label.h>
+#include <gtkmm/listviewtext.h>
+#include <gtkmm/scrolledwindow.h>
+
+#include "case.h"
+
+// dialog used to search blocks
+class FindDialog: public Gtk::Dialog {
+	public:
+		// constructor
+		FindDialog(const BufferMap &buffers);
+		
+		// get the selected block
+		Glib::ustring get_selected() { return m_ResultsList->get_text(m_ResultsList->get_selected()[0], 0); }
+		
+	private:
+		// build the ui
+		void construct();
+		
+		// search button click handler
+		void on_search_clicked();
+		
+		// query text changed handler
+		void on_entry_text_changed();
+		
+		// list selection change handler
+		void on_selection_changed();
+		
+		// selected block
+		Glib::ustring m_CurBlock;
+		
+		// labels
+		Gtk::Label *m_QueryLabel;
+		Gtk::Label *m_ResultsLabel;
+		
+		// buttons
+		Gtk::Button *m_SearchButton;
+		Gtk::Button *m_GoButton;
+		
+		// enties
+		Gtk::Entry *m_QueryEntry;
+		
+		// containers
+		Gtk::ScrolledWindow *m_SWindow;
+		
+		// listview for results
+		Gtk::ListViewText *m_ResultsList;
+		
+		// blocks
+		BufferMap m_Buffers;
+};
+
+#endif
